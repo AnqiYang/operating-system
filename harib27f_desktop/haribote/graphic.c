0fbc81d7719044501eb5a523a569c888da25db57
AnqiYang <1598494656@qq.com>
2015-07-21 09:54:53 +0800

    desktop
    We intended to add pictures to the desktop, but we still have some bugs
    in reading files.

---
+++ harib27f_desktop/haribote/graphic.c
@@ -0,0 +1,340 @@
+/* 僌儔僼傿僢僋張棟娭學 */
+
+#include "bootpack.h"
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:崟 */
+		0xff, 0x00, 0x00,	/*  1:柧傞偄愒 */
+		0x00, 0xff, 0x00,	/*  2:柧傞偄椢 */
+		0xff, 0xff, 0x00,	/*  3:柧傞偄墿怓 */
+		0x00, 0x00, 0xff,	/*  4:柧傞偄惵 */
+		0xff, 0x00, 0xff,	/*  5:柧傞偄巼 */
+		0x00, 0xff, 0xff,	/*  6:柧傞偄悈怓 */
+		0xff, 0xff, 0xff,	/*  7:敀 */
+		0xc6, 0xc6, 0xc6,	/*  8:柧傞偄奃怓 */
+		0x84, 0x00, 0x00,	/*  9:埫偄愒 */
+		0x00, 0x84, 0x00,	/* 10:埫偄椢 */
+		0x84, 0x84, 0x00,	/* 11:埫偄墿怓 */
+		0x00, 0x00, 0x84,	/* 12:埫偄惵 */
+		0x84, 0x00, 0x84,	/* 13:埫偄巼 */
+		0x00, 0x84, 0x84,	/* 14:埫偄悈怓 */
+		0x84, 0x84, 0x84	/* 15:埫偄奃怓 */
+	};
+	unsigned char table2[216 * 3];
+	int r, g, b;
+	set_palette(0, 15, table_rgb);
+	for (b = 0; b < 6; b++) {
+		for (g = 0; g < 6; g++) {
+			for (r = 0; r < 6; r++) {
+				table2[(r + g * 6 + b * 36) * 3 + 0] = r * 51;
+				table2[(r + g * 6 + b * 36) * 3 + 1] = g * 51;
+				table2[(r + g * 6 + b * 36) * 3 + 2] = b * 51;
+			}
+		}
+	}
+	set_palette(16, 231, table2);
+	return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();	/* 妱傝崬傒嫋壜僼儔僌偺抣傪婰榐偡傞 */
+	io_cli(); 					/* 嫋壜僼儔僌傪0偵偟偰妱傝崬傒嬛巭偵偡傞 */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);	/* 妱傝崬傒嫋壜僼儔僌傪尦偵栠偡 */
+	return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++)
+			vram[y * xsize + x] = c;
+	}
+	return;
+}
+
+void init_screen8(char *vram, int x, int y)
+{
+	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
+	boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
+
+	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
+	boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
+	boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
+	boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
+	boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
+	boxfill8(vram, x, COL8_000000, 60,     y - 24, 60,     y -  3);
+
+	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
+	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
+	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
+	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
+	return;
+}
+
+
+
+void init_screen9(char *vram, int x, int y)
+{
+	struct DLL_STRPICENV {	/* 64KB */
+		int work[64 * 1024 / 4];
+	};
+
+	struct RGB {
+		unsigned char b, g, r, t;
+	};
+
+	/* jpeg.c */
+	int info_JPEG(struct DLL_STRPICENV *env, int *info, int size, char *fp);
+	int decode0_JPEG(struct DLL_STRPICENV *env, int size, char *fp, int b_type, char *buf, int skip);
+
+	unsigned char rgb2pal(int r, int g, int b, int x, int y);
+	void error(char *s);
+
+	struct DLL_STRPICENV env;
+	char filebuf[512 * 1024];
+	char *p;
+	int win, i, j, fsize, xsize, info[8];
+	struct RGB picbuf[1024 * 768], *q;
+
+	boxfill8(vram, x, COL8_008484, 0, 0, x - 1, y - 29);
+	boxfill8(vram, x, COL8_C6C6C6, 0, y - 28, x - 1, y - 28);
+	boxfill8(vram, x, COL8_FFFFFF, 0, y - 27, x - 1, y - 27);
+	boxfill8(vram, x, COL8_C6C6C6, 0, y - 26, x - 1, y - 1);
+
+	boxfill8(vram, x, COL8_FFFFFF, 3, y - 24, 59, y - 24);
+	boxfill8(vram, x, COL8_FFFFFF, 2, y - 24, 2, y - 4);
+	boxfill8(vram, x, COL8_848484, 3, y - 4, 59, y - 4);
+	boxfill8(vram, x, COL8_848484, 59, y - 23, 59, y - 5);
+	boxfill8(vram, x, COL8_000000, 2, y - 3, 59, y - 3);
+	boxfill8(vram, x, COL8_000000, 60, y - 24, 60, y - 3);
+
+	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x - 4, y - 24);
+	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y - 4);
+	boxfill8(vram, x, COL8_FFFFFF, x - 47, y - 3, x - 4, y - 3);
+	boxfill8(vram, x, COL8_FFFFFF, x - 3, y - 24, x - 3, y - 3);
+
+
+	/* 文件载入 */
+	char s[] = "fujisan.jpg";
+	p = s;
+	i = api_fopen(p); 
+	error("I'm Here");
+	if (i == 0) { error("file not found.\n"); }
+	fsize = api_fsize(i, 0);
+	if (fsize > 512 * 1024) {
+		error("file too large.\n");
+	}
+	api_fread(filebuf, fsize, i);
+	api_fclose(i);
+
+	//
+	//if (info_JPEG(&env, info, fsize, filebuf) == 0) {
+	//		/* JPEG偱傕側偐偭偨 */
+	//	api_putstr0("file type unknown.\n");
+	//	api_end();
+	//}
+	//
+	///* 偳偪傜偐偺info娭悢偑惉岟偡傞偲丄埲壓偺忣曬偑info偵擖偭偰偄傞 */
+	///*	info[0] : 僼傽僀儖僞僀僾 (1:BMP, 2:JPEG) */
+	///*	info[1] : 僇儔乕忣曬 */
+	///*	info[2] : xsize */
+	///*	info[3] : ysize */
+
+
+	///* 将文件内容转换为图像数据 */
+
+	//i = decode0_JPEG(&env, fsize, filebuf, 4, (char *)picbuf, 0);
+
+	///* b_type = 4 表示 struct RGB 格式 */
+	///* skip设为0即可 */
+	//if (i != 0) {
+	//	error("decode error.\n");
+	//}
+
+
+	///* 显示 */
+	//
+
+	//for (i = 0; i < info[3]; i++) {
+	//	/*p = winbuf + (i + 29) * xsize + (xsize - info[2]) / 2;*/
+	//	p = vram + i*x + (x - info[2]) / 2;
+	//	q = picbuf + i * info[2];
+	//	for (j = 0; j < info[2]; j++) {
+	//		p[j] = rgb2pal(q[j].r, q[j].g, q[j].b, j, i);
+	//	}
+	//}
+
+}
+
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p, d /* data */;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; }
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	struct TASK *task = task_now();
+	char *nihongo = (char *) *((int *) 0x0fe8), *font;
+	int k, t;
+
+	if (task->langmode == 0) {
+		for (; *s != 0x00; s++) {
+			putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+			x += 8;
+		}
+	}
+	if (task->langmode == 1) {
+		for (; *s != 0x00; s++) {
+			if (task->langbyte1 == 0) {
+				if ((0x81 <= *s && *s <= 0x9f) || (0xe0 <= *s && *s <= 0xfc)) {
+					task->langbyte1 = *s;
+				} else {
+					putfont8(vram, xsize, x, y, c, nihongo + *s * 16);
+				}
+			} else {
+				if (0x81 <= task->langbyte1 && task->langbyte1 <= 0x9f) {
+					k = (task->langbyte1 - 0x81) * 2;
+				} else {
+					k = (task->langbyte1 - 0xe0) * 2 + 62;
+				}
+				if (0x40 <= *s && *s <= 0x7e) {
+					t = *s - 0x40;
+				} else if (0x80 <= *s && *s <= 0x9e) {
+					t = *s - 0x80 + 63;
+				} else {
+					t = *s - 0x9f;
+					k++;
+				}
+				task->langbyte1 = 0;
+				font = nihongo + 256 * 16 + (k * 94 + t) * 32;
+				putfont8(vram, xsize, x - 8, y, c, font     );	/* 嵍敿暘 */
+				putfont8(vram, xsize, x    , y, c, font + 16);	/* 塃敿暘 */
+			}
+			x += 8;
+		}
+	}
+	if (task->langmode == 2) {
+		for (; *s != 0x00; s++) {
+			if (task->langbyte1 == 0) {
+				if (0x81 <= *s && *s <= 0xfe) {
+					task->langbyte1 = *s;
+				} else {
+					putfont8(vram, xsize, x, y, c, nihongo + *s * 16);
+				}
+			} else {
+				k = task->langbyte1 - 0xa1;
+				t = *s - 0xa1;
+				task->langbyte1 = 0;
+				font = nihongo + 256 * 16 + (k * 94 + t) * 32;
+				putfont8(vram, xsize, x - 8, y, c, font     );	/* 嵍敿暘 */
+				putfont8(vram, xsize, x    , y, c, font + 16);	/* 塃敿暘 */
+			}
+			x += 8;
+		}
+	}
+	return;
+}
+
+void init_mouse_cursor8(char *mouse, char bc)
+/* 儅僂僗僇乕僜儖傪弨旛乮16x16乯 */
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = bc;
+			}
+		}
+	}
+	return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize)
+{
+	int x, y;
+	for (y = 0; y < pysize; y++) {
+		for (x = 0; x < pxsize; x++) {
+			vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+		}
+	}
+	return;
+}
+
+unsigned char rgb2pal(int r, int g, int b, int x, int y)
+{
+	static int table[4] = { 3, 1, 0, 2 };
+	int i;
+	x &= 1; /* 嬼悢偐婏悢偐 */
+	y &= 1;
+	i = table[x + y * 2];	/* 拞娫怓傪嶌傞偨傔偺掕悢 */
+	r = (r * 21) / 256;	/* 偙傟偱 0乣20 偵側傞 */
+	g = (g * 21) / 256;
+	b = (b * 21) / 256;
+	r = (r + i) / 4;	/* 偙傟偱 0乣5 偵側傞 */
+	g = (g + i) / 4;
+	b = (b + i) / 4;
+	return 16 + r + g * 6 + b * 36;
+}
+
+
+void error(char *s)
+{
+	api_putstr0(s);
+	api_end();
+}
